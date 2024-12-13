17cbd7ced246155538efe5f63c961778cb42576e
Adrian Cardenas <92567314+Hcardenass@users.noreply.github.com>
2024-12-13 13:32:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let header = document.querySelector("header");
+let navegacion = document.querySelector("nav");
+let tarjeta = document.querySelector("tarjetas");
+let footer = document.querySelector("footer");
+
+
+console.log(header);
+console.log(navegacion);
+console.log(tarjeta);
+console.log(footer);
